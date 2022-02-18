8e4504b1ca55bad4de09fed79a8df1ab55cee5d2
Nishant Sinha <Nishant.Sinha@Rsystems.com>
2022-02-18 10:07:41 +0530

    Java interview Program

---
+++ JavaPractics/JavaSeleniumInterviewSet/src/main/java/org/excel/util/Runner.java
@@ -0,0 +1,14 @@
+package org.excel.util;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		
+		ExcelReader reader=new ExcelReader();
+		reader.getAllData();
+		
+
+	}
+
+}
